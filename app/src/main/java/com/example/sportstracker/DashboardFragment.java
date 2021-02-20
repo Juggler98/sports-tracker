8e11274af1a6a399b344e3c5e808af3c57159ba3
Juggler98 <a.beliansky98@gmail.com>
2021-02-20 22:30:21 +0100

    Dashboard Cards

--- app/src/main/java/com/example/sportstracker/DashboardFragment.java
+++ app/src/main/java/com/example/sportstracker/DashboardFragment.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import static com.example.sportstracker.MainActivity.EXTRA;
@@ -18,11 +19,10 @@ import static com.example.sportstracker.MainActivity.NAME_OF_ACTIVITY;
 import static com.example.sportstracker.MainActivity.RECORDING_PREF;
 import static com.example.sportstracker.MainActivity.SHARED_PREFERENCES;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements View.OnClickListener {
 
+    private CardView recordCard, activitiesCard, statsCard;
 
-
-    private Button buttonRecord;
     private int routeID = 0;
     private boolean newActivity = true;
     private SharedPreferences sharedPreferences;
@@ -35,9 +35,13 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.dashboard_fragment, container, false);
 
-        Button buttonActivities = view.findViewById(R.id.button2);
-        Button buttonStats = view.findViewById(R.id.button3);
-        Button buttonRecord = view.findViewById(R.id.button1);
+        recordCard = view.findViewById(R.id.record_card);
+        activitiesCard = view.findViewById(R.id.activities_card);
+        statsCard = view.findViewById(R.id.stats_card);
+
+        recordCard.setOnClickListener(this);
+        activitiesCard.setOnClickListener(this);
+        statsCard.setOnClickListener(this);
 
         sharedPreferences = this.getActivity().getSharedPreferences(SHARED_PREFERENCES, getContext().MODE_PRIVATE);
         newActivity = sharedPreferences.getBoolean(RECORDING_PREF, true);
@@ -45,55 +49,53 @@ public class DashboardFragment extends Fragment {
 
         database = new Database(getContext());
 
-        buttonStats.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(view.getContext(), StatsActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        buttonActivities.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(view.getContext(), RoutesActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        buttonRecord.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                newActivity = sharedPreferences.getBoolean(RECORDING_PREF, true);
-                if (newActivity) {
-
-                    double time = System.currentTimeMillis();
-                    Activity activity = new Activity(1, time);
-                    database.createActivity(activity);
-
-                    newActivity = false;
-                    routeID = database.getLastActivityID();
-
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putBoolean(RECORDING_PREF, newActivity);
-                    editor.putInt(NAME_OF_ACTIVITY, routeID);
-                    editor.apply();
-
-                    Intent intent = new Intent(view.getContext(), ServiceGPS.class);
-                    intent.putExtra(EXTRA, routeID);
-                    getContext().startService(intent);
-
-                    Log.d("RECORD_LC", "Starting new Service GPS");
-                }
-                Intent intent = new Intent(getContext(), RecordActivity.class);
-                startActivity(intent);
-            }
-        });
-
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent;
 
+        switch (v.getId()) {
+            case R.id.record_card:
+                record(v);
+                break;
+            case R.id.activities_card:
+                intent = new Intent(v.getContext(), RoutesActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.stats_card:
+                intent = new Intent(v.getContext(), StatsActivity.class);
+                startActivity(intent);
+                break;
 
+        }
+    }
+
+    private void record(View view) {
+        newActivity = sharedPreferences.getBoolean(RECORDING_PREF, true);
+        if (newActivity) {
+
+            double time = System.currentTimeMillis();
+            Activity activity = new Activity(1, time);
+            database.createActivity(activity);
+
+            newActivity = false;
+            routeID = database.getLastActivityID();
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(RECORDING_PREF, newActivity);
+            editor.putInt(NAME_OF_ACTIVITY, routeID);
+            editor.apply();
+
+            Intent intent = new Intent(view.getContext(), ServiceGPS.class);
+            intent.putExtra(EXTRA, routeID);
+            getContext().startService(intent);
+
+            Log.d("RECORD_LC", "Starting new Service GPS");
+        }
+        Intent intent = new Intent(getContext(), RecordActivity.class);
+        startActivity(intent);
+    }
 
 }
