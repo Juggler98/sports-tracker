3b8751f46d095b019f5dc2d7c9cd5e401d495bc8
Juggler98 <a.beliansky98@gmail.com>
2021-03-01 22:58:30 +0100

    Recycler View

---
+++ app/src/main/java/com/example/sportstracker/RouteAdapter.java
@@ -0,0 +1,76 @@
+package com.example.sportstracker;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.RouteViewHolder> {
+    private ArrayList<RouteItem> routeItems;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public static class RouteViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView textView1;
+        public TextView textView2;
+
+        public RouteViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            textView1 = itemView.findViewById(R.id.textView1);
+            textView2 = itemView.findViewById(R.id.textView2);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public RouteAdapter(ArrayList<RouteItem> routeItems) {
+        this.routeItems = routeItems;
+    }
+
+    @NonNull
+    @Override
+    public RouteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.route_item, parent, false);
+        RouteViewHolder routeViewHolder = new RouteViewHolder(view, listener);
+        return routeViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RouteViewHolder holder, int position) {
+        RouteItem currentItem = routeItems.get(position);
+
+        holder.imageView.setImageResource(currentItem.getIcon());
+        holder.textView1.setText(currentItem.getDate());
+        holder.textView2.setText(currentItem.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return routeItems.size();
+    }
+}
--- app/src/main/java/com/example/sportstracker/RoutesActivity.java
+++ app/src/main/java/com/example/sportstracker/RoutesActivity.java
@@ -1,6 +1,8 @@
 package com.example.sportstracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -26,76 +28,98 @@ import static com.example.sportstracker.RouteInfoActivity.IS_RELOAD_NEEDED;
 public class RoutesActivity extends AppCompatActivity {
 
     private RoutesMethods routesMethods = new RoutesMethods();
+    private Database database;
 
     private ListView listView;
 
     private ArrayList<String> arrayListListView = new ArrayList<>();
     private ArrayList<Activity> activities = new ArrayList<>();
 
-    private Database database;
+    private RecyclerView recyclerView;
+    private RouteAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private ArrayList<RouteItem> routeItemsList = new ArrayList<>();
+
+    private int activityOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
+        Log.d("Routes_LC", "onCreate Routes");
 
         database = new Database(RoutesActivity.this);
         activities = database.getActivities();
 
+//        listView = findViewById(R.id.listView);
 
-//        final RoutesMethods routesMethods = new RoutesMethods();
+        for (Activity activity : activities) {
+            routeItemsList.add(new RouteItem(R.drawable.ic_hike,routesMethods.getDate(activity.getTimeStart()), activity.getTitle()));
+        }
 
-        Log.d("Routes_LC", "onCreate Routes");
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new RouteAdapter(routeItemsList);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
 
-        listView = findViewById(R.id.listView);
+        adapter.setOnItemClickListener(new RouteAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                openStats(activities.get(position).getId());
+                activityOpen = position;
+            }
+        });
 
         loadListView();
-        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayListListView));
+//        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayListListView));
 
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                final int positionFinal = position;
-                AlertDialog.Builder builder = new AlertDialog.Builder(RoutesActivity.this);
-                builder.setMessage("Delete this activity?").setCancelable(true).setPositiveButton("Delete", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        database.deleteActivity(activities.get(position).getId());
-                        activities = database.getActivities();
-                        loadListView();
-                        listView.setAdapter(new ArrayAdapter<>(RoutesActivity.this, android.R.layout.simple_list_item_1, arrayListListView));
-                    }
-                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                AlertDialog alertDialog = builder.create();
-                alertDialog.show();
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+//                final int positionFinal = position;
+//                AlertDialog.Builder builder = new AlertDialog.Builder(RoutesActivity.this);
+//                builder.setMessage("Delete this activity?").setCancelable(true).setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        database.deleteActivity(activities.get(position).getId());
+//                        activities = database.getActivities();
+//                        loadListView();
+//                        listView.setAdapter(new ArrayAdapter<>(RoutesActivity.this, android.R.layout.simple_list_item_1, arrayListListView));
+//                    }
+//                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.cancel();
+//                    }
+//                });
+//                AlertDialog alertDialog = builder.create();
+//                alertDialog.show();
+//
+//                return true;
+//            }
+//        });
 
-                return true;
-            }
-        });
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                openStats(activities.get(position).getId());
+//            }
+//        });
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                openStats(activities.get(position).getId());
-            }
-        });
-
-        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+//        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
 
 
     }
@@ -118,13 +142,21 @@ public class RoutesActivity extends AppCompatActivity {
         Log.d("Routes_LC", "onResume Routes");
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, MODE_PRIVATE);
         boolean isReloadNeeded = sharedPreferences.getBoolean(IS_RELOAD_NEEDED, false);
+        int oldSize = activities.size();
         if (isReloadNeeded) {
             activities = database.getActivities();
-            loadListView();
-            listView.setAdapter(new ArrayAdapter<>(RoutesActivity.this, android.R.layout.simple_list_item_1, arrayListListView));
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putBoolean(IS_RELOAD_NEEDED, false);
-            editor.apply();
+            if (activities.size() < oldSize) {
+                adapter.notifyItemRemoved(activityOpen);
+            } else {
+                routeItemsList.get(activityOpen).setTitle(activities.get(activityOpen).getTitle());
+                adapter.notifyItemChanged(activityOpen);
+            }
+//            activities = database.getActivities();
+//            loadListView();
+//            listView.setAdapter(new ArrayAdapter<>(RoutesActivity.this, android.R.layout.simple_list_item_1, arrayListListView));
+//            SharedPreferences.Editor editor = sharedPreferences.edit();
+//            editor.putBoolean(IS_RELOAD_NEEDED, false);
+//            editor.apply();
         }
 
     }
@@ -142,15 +174,15 @@ public class RoutesActivity extends AppCompatActivity {
     }
 
     private void loadListView() {
-        arrayListListView.clear();
-        for (int i = 0; i < activities.size(); ++i) {
-            Activity activity = activities.get(i);
-            if (activity.getTitle().equals("")) {
-                arrayListListView.add((i + 1) + ". " + routesMethods.getDate(activity.getTimeStart()));
-            } else {
-                arrayListListView.add((i + 1) + ". " + activity.getTitle());
-            }
-        }
+//        arrayListListView.clear();
+//        for (int i = 0; i < activities.size(); ++i) {
+//            Activity activity = activities.get(i);
+//            if (activity.getTitle().equals("")) {
+//                arrayListListView.add((i + 1) + ". " + routesMethods.getDate(activity.getTimeStart()));
+//            } else {
+//                arrayListListView.add((i + 1) + ". " + activity.getTitle());
+//            }
+//        }
     }
 
     private void openStats(int activityID) {
