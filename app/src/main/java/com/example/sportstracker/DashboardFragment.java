5376e1eceb2512eb1ec47173bca2ba0f8cbbf9bd
Juggler98 <a.beliansky98@gmail.com>
2021-03-13 13:50:18 +0100

    Dashboard Warnings Solved

--- app/src/main/java/com/example/sportstracker/DashboardFragment.java
+++ app/src/main/java/com/example/sportstracker/DashboardFragment.java
@@ -2,6 +2,7 @@ package com.example.sportstracker;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -24,24 +25,22 @@ import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment implements View.OnClickListener {
 
-    private CardView recordCard, activitiesCard, statsCard;
-
-    private int routeID = 0;
-    private boolean newActivity = true;
-    private SharedPreferences sharedPreferences;
     private SharedPreferences defaultSharedPreferences;
+    private SharedPreferences sharedPreferences;
+    private boolean newRoute = true;
+    private int routeType = 1;
+    private int routeID = 0;
 
     private Database database;
 
-    private int activityType = 1;
-
-    private LoadingDialog loadingDialog;
-
+    private Context context;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.dashboard_fragment, container, false);
+
+        CardView recordCard, activitiesCard, statsCard;
 
         recordCard = view.findViewById(R.id.record_card);
         activitiesCard = view.findViewById(R.id.activities_card);
@@ -51,15 +50,14 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
         activitiesCard.setOnClickListener(this);
         statsCard.setOnClickListener(this);
 
-        sharedPreferences = this.getActivity().getSharedPreferences(getString(R.string.sharedPreferences), getContext().MODE_PRIVATE);
-        newActivity = sharedPreferences.getBoolean(getString(R.string.recordingPref), true);
+        context = requireContext();
+
+        defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        sharedPreferences = context.getSharedPreferences(getString(R.string.sharedPreferences), Context.MODE_PRIVATE);
+        newRoute = sharedPreferences.getBoolean(getString(R.string.recordingPref), true);
         routeID = sharedPreferences.getInt(getString(R.string.routeNamePref), 0);
 
-        defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-
-        database = new Database(getContext());
-
-        loadingDialog = new LoadingDialog(getActivity(), true);
+        database = new Database(context);
 
         return view;
     }
@@ -67,19 +65,17 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
     @Override
     public void onResume() {
         super.onResume();
-        activityType = Integer.parseInt(defaultSharedPreferences.getString(getString(R.string.routeTypePref),"1"));
-        loadingDialog.dismissDialog();
+        routeType = Integer.parseInt(defaultSharedPreferences.getString(getString(R.string.routeTypePref), "1"));
     }
 
     @Override
     public void onClick(View v) {
         Intent intent;
-
         switch (v.getId()) {
             case R.id.record_card:
                 if (ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    newActivity = sharedPreferences.getBoolean(getString(R.string.recordingPref), true);
-                    if (newActivity) {
+                    newRoute = sharedPreferences.getBoolean(getString(R.string.recordingPref), true);
+                    if (newRoute) {
                         createActivityTypeDialog();
                     } else {
                         openMap();
@@ -91,38 +87,35 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
                 startActivity(intent);
                 break;
             case R.id.stats_card:
-                //loadingDialog.startLoadingDialog();
                 intent = new Intent(v.getContext(), StatsActivity.class);
                 startActivity(intent);
                 break;
-
         }
     }
 
     // boolean new activity permit start GPS service only one time, until stop
     private void record(View view) {
-        newActivity = sharedPreferences.getBoolean(getString(R.string.recordingPref), true);
-        if (newActivity) {
-
+        newRoute = sharedPreferences.getBoolean(getString(R.string.recordingPref), true);
+        if (newRoute) {
             double time = System.currentTimeMillis();
-            Route route = new Route(activityType, time);
+            Route route = new Route(routeType, time);
 
-            boolean autoPause = defaultSharedPreferences.getBoolean(getString(R.string.autoPausePref),true);
+            boolean autoPause = defaultSharedPreferences.getBoolean(getString(R.string.autoPausePref), true);
             route.setAutoPause(autoPause);
 
             database.createActivity(route);
 
-            newActivity = false;
+            newRoute = false;
             routeID = database.getLastActivityID();
 
             SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putBoolean(getString(R.string.recordingPref), newActivity);
+            editor.putBoolean(getString(R.string.recordingPref), newRoute);
             editor.putInt(getString(R.string.routeNamePref), routeID);
             editor.apply();
 
             Intent intent = new Intent(view.getContext(), ServiceGPS.class);
             intent.putExtra(getString(R.string.intentExtra), routeID);
-            getContext().startService(intent);
+            context.startService(intent);
 
             Log.d("RECORD_LC", "Starting new Service GPS");
         }
@@ -143,12 +136,11 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
         String[] types = new String[arrayList.size()];
         types = arrayList.toArray(types);
 
-//        activityType = 1;
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Select Activity Type").setSingleChoiceItems(types, activityType - 1, new DialogInterface.OnClickListener() {
+        builder.setTitle("Select Activity Type").setSingleChoiceItems(types, routeType - 1, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                activityType = which + 1;
+                routeType = which + 1;
             }
         }).setPositiveButton("START", new DialogInterface.OnClickListener() {
             @Override
